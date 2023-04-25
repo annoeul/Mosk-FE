f143d7326644e81b78a11502f55f12b8bc2a4331
annoeul <shdmf1137@gmail.com>
2023-04-26 01:06:53 +0900

    Conf: 초기 설정

---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import Home from "../../components/home"
+
+function HomePage() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default HomePage
