e68aff947d78137b230b9fec303885be180a7115
annoeul <shdmf1137@gmail.com>
2023-04-26 22:49:02 +0900

    Desgin : 디자인 중

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,15 @@
-import React from "react"
+import { useState } from "react"
+import * as S from "./style"
+import { Link } from "react-router-dom"
 
 function LoginForm() {
-  return <div>LoginForm</div>
+  return (
+    <S.Form>
+      <S.Label>
+        <S.Input></S.Input>
+      </S.Label>
+    </S.Form>
+  )
 }
 
 export default LoginForm
