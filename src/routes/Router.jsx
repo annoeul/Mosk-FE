31efcea51b9018563b7439cde72667906e89fd02
annoeul <shdmf1137@gamil.com>
2023-04-27 14:41:12 +0900

    Feat: Register Route 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/loginPage"
+import RegisterPage from "../pages/registerPage"
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
