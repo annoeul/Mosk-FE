7e3fe3716c71a0efce5f645934def9062e5a33e0
annoeul <shdmf1137@gmail.com>
2023-04-27 21:19:54 +0900

    Feat: Home route 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/loginPage"
 import RegisterPage from "../pages/registerPage"
+import HomePage from "../pages/homePage"
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
