fca696b3027f88c317cc14ff5f683fe4b926a017
annoeul <shdmf1137@gmail.com>
2023-04-29 23:18:50 +0900

    Design: 회원가입 레이아웃 1차 완료

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import * as S from "./style"
 
 function RegisterForm() {
-  return <div>RegisterForm</div>
+  return (
+    <S.Wrapper>
+      <S.FormContainer>
+        <S.LoginForm>
+          <S.FormTitle>Signup</S.FormTitle>
+          <S.Input type="text" placeholder="ID" />
+          <S.Input type="password" placeholder="Password" />
+          <S.Input type="password" placeholder="Password" />
+          {/* <S.Input type="password" placeholder="Password" /> */}
+          <S.SubmitButton>다음 단계</S.SubmitButton>
+        </S.LoginForm>
+      </S.FormContainer>
+    </S.Wrapper>
+  )
 }
 
 export default RegisterForm
