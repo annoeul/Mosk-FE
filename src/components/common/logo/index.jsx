1b20ed49e00824ff9684a43ecb6e4559449f5a43
annoeul <shdmf1137@gmail.com>
2023-04-29 21:17:05 +0900

    Feat: logo components 생성

---
+++ src/components/common/logo/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import * as S from "./style"
+import { Container } from "react-bootstrap"
+
+function Logo() {
+  return (
+    <Container>
+      <S.Logo src="img/logo.png" />
+    </Container>
+  )
+}
+
+export default Logo
---
+++ src/components/common/logo/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Logo = styled.img`
+  /* width: 150px; */
+  /* height: 200px; */
+  margin: 0 auto;
+`
