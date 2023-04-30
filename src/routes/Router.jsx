5a991577dae8d0babfd1eed53636a903013dfeb5
annoeul <shdmf1137@gmail.com>
2023-04-30 23:53:53 +0900

    Feat: 사업자 등록증 페이지 route 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/loginPage"
 import RegisterPage from "../pages/registerPage"
 import HomePage from "../pages/homePage"
+import StoreRegisterPage from "../pages/storeRegisterPage"
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register/store" element={<StoreRegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
