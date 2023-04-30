b82150b9f33f059acf59fdcd6f919f9b1ec2f5fa
annoeul <shdmf1137@gmail.com>
2023-05-01 00:24:26 +0900

    Style: loginForm 수정

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,47 +1,32 @@
 import React from "react"
-import { Box, Button, Container, FormControlLabel, Grid, Link, TextField, Typography } from "@mui/material"
-import { CheckBox } from "@mui/icons-material"
-import Logo from "../common/logo"
+import { Box, Button, Container, FormControlLabel, TextField, Typography } from "@mui/material"
+// import { CheckBox } from "@mui/icons-material"
+import { Link } from "react-router-dom"
 
 function LoginForm() {
   return (
-    <Container component="main" maxWidth="md">
+    <Container component="main" maxWidth="sm">
       <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
         <Typography component="h1" variant="h5">
           Login
         </Typography>
-        <TextField
-          name="email"
-          label="Email Address"
-          required
-          fullWidth
-          autoComplete="email"
-          margin="normal"
-          autoFocus
-        />
+        <TextField name="email" label="이메일" required fullWidth autoComplete="email" margin="normal" autoFocus />
         <TextField
           name="password"
-          label="password"
+          label="비밀번호"
           type="password"
           required
           fullWidth
           autoComplete="current-password"
           margin="normal"
         />
-        <FormControlLabel control={<CheckBox value="remember" color="primary" />} label="Remember me" />
-        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        {/* <FormControlLabel control={<CheckBox value="remember" color="primary" />} label="Remember me" /> */}
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} style={{ height: "50px" }}>
           login
         </Button>
-        {/* <Grid container>
-          <Grid item xs>
-            <Link to="#">Forgot password?</Link>
-          </Grid>
-          <Grid item>
-            <Link to="register" variant="body2">
-              Sign Up
-            </Link>
-          </Grid>
-        </Grid> */}
+        <span>
+          아직 회원이 아니신가요?<Link to="/register">회원가입</Link>
+        </span>
       </Box>
     </Container>
   )
