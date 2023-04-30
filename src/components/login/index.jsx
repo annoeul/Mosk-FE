de333482687fc49c0382da834750f6b6b4f4eab7
annoeul <shdmf1137@gmail.com>
2023-04-30 16:39:59 +0900

    Design: login layout

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,22 +1,49 @@
 import React from "react"
-import * as S from "./style"
-import { Link } from "react-router-dom"
+import { Box, Button, Container, FormControlLabel, Grid, Link, TextField, Typography } from "@mui/material"
+import { CheckBox } from "@mui/icons-material"
+import Logo from "../common/logo"
 
 function LoginForm() {
   return (
-    <S.Wrapper>
-      <S.FormContainer>
-        <S.LoginForm>
-          <S.FormTitle>Login</S.FormTitle>
-          <S.Input type="text" placeholder="ID" />
-          <S.Input type="password" placeholder="Password" />
-          <S.SubmitButton>Login</S.SubmitButton>
-          <p>
-            회원이 아니신가요? <Link to="/register">회원가입</Link>
-          </p>
-        </S.LoginForm>
-      </S.FormContainer>
-    </S.Wrapper>
+    <Container component="main" maxWidth="md">
+      <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <TextField
+          name="email"
+          label="Email Address"
+          required
+          fullWidth
+          autoComplete="email"
+          margin="normal"
+          autoFocus
+        />
+        <TextField
+          name="password"
+          label="password"
+          type="password"
+          required
+          fullWidth
+          autoComplete="current-password"
+          margin="normal"
+        />
+        <FormControlLabel control={<CheckBox value="remember" color="primary" />} label="Remember me" />
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          login
+        </Button>
+        {/* <Grid container>
+          <Grid item xs>
+            <Link to="#">Forgot password?</Link>
+          </Grid>
+          <Grid item>
+            <Link to="register" variant="body2">
+              Sign Up
+            </Link>
+          </Grid>
+        </Grid> */}
+      </Box>
+    </Container>
   )
 }
 
