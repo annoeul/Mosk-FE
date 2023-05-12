b96403ae0e791cc5e01a1d60ac2847b0f7265ca9
annoeul <shdmf1137@gmail.com>
2023-05-13 01:42:31 +0900

    Feat: Routes 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LoginPage from "../pages/loginPage"
-import RegisterPage from "../pages/registerPage"
-import HomePage from "../pages/homePage"
-import StoreRegisterPage from "../pages/storeRegisterPage"
+import KioskMain from "../components/kioskmain/index"
+
+// import LoginPage from "../pages/loginPage"
+// import RegisterPage from "../pages/registerPage"
+// import HomePage from "../pages/homePage"
+// import StoreRegisterPage from "../pages/storeRegisterPage"
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/register/store" element={<StoreRegisterPage />} />
+        <Route path="/register/store" element={<StoreRegisterPage />} />  */}
+        <Route path="/" element={<KioskMain />} />
       </Routes>
     </BrowserRouter>
   )
