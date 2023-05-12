1770d15b7f431e479184ce280cb24f9d5f8a7032
annoeul <shdmf1137@gmail.com>
2023-05-13 01:39:39 +0900

    Feat: create menu

---
+++ src/components/menu/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const MenuWrapper = styled.div`
+  background-color: yellow;
+  height: 100vh;
+  margin-top: 15px;
+  padding-bottom: 10px;
+
+  /* width: 100vw; */
+`
