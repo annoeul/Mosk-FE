d09205076bd756029b3bec6bc8591e93e826db8c
annoeul <shdmf1137@gmail.com>
2023-05-13 01:41:56 +0900

    Feat: Create mockdata

---
+++ src/data/one.js
@@ -0,0 +1,31 @@
+export const one = [
+  {
+    id: 1,
+    name: "메뉴명1",
+    image: new URLSearchParams("https://picsum.photos/100/100"),
+    price: "1000",
+    description: "메뉴1설명",
+  },
+  {
+    id: 2,
+    name: "메뉴명2",
+    image: new URL("https://picsum.photos/100/100"),
+    price: "2000",
+    description: "메뉴2설명",
+  },
+  {
+    id: 3,
+    name: "메뉴명3",
+    image: new URL("https://picsum.photos/100/100"),
+    price: "3000",
+    description: "메뉴3설명",
+  },
+  {
+    id: 1,
+    name: "메뉴명4",
+    image: new URL("https://picsum.photos/100/100"),
+    price: "4000",
+    description: "메뉴4설명",
+  },
+]
+export default one
