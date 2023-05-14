82d30581e76dc88fc3af0bc428c2289858e6a469
annoeul <shdmf1137@gmail.com>
2023-05-15 01:11:18 +0900

    Feat: 데이터 가져오기

--- src/pages/kioskmain/index.jsx
+++ src/pages/kioskmain/index.jsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from "react"
-import Logo from "../../components/common/logo/index"
-import { Container } from "@material-ui/core"
-
-import Menu from "../../components/menu/index"
-import Category from "../../components/category"
+// MainKiosk.jsx
+import React, { useState, useEffect } from "react"
 import axios from "axios"
+import { Container } from "@material-ui/core"
+import Logo from "../../components/common/logo"
+import Category from "../../components/category"
+import Menu from "../../components/menu"
 import Bottom from "../../components/bottom"
 
 function KioskMain() {
-  const [items, setItems] = useState()
+  const [items, setItems] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("")
 
   const getData = async () => {
-    const response = await axios.get("http://localhost:8000/menu")
-
-    setItems(response.data)
+    try {
+      const response = await axios.get("http://localhost:8000/menu")
+      setItems(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  // console.log("items = " + items)
 
   useEffect(() => {
     getData()
   }, [])
 
+  const handleCategoryChange = (categoryId) => {
+    setSelectedCategory(categoryId)
+  }
+
   return (
-    <Container style={{ maxWidth: "425px", backgroundColor: "grey" }}>
+    <Container style={{ maxWidth: "425px", backgroundColor: "#5df0c6" }}>
       <Logo size={40} />
-      <Category />
-      <Menu items={items} />
-      {/* <Outlet /> */}
+      <Category items={items} selectedCategory={selectedCategory} onChange={handleCategoryChange} />
+      <Menu items={items} selectedCategory={selectedCategory} />
       <Bottom />
     </Container>
   )
