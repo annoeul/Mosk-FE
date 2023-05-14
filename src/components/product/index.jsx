62eb17ec8b0f576af0673ed02e36eaf3ec070ef2
annoeul <shdmf1137@gmail.com>
2023-05-15 02:26:58 +0900

    Feat: options modal 추가

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -1,17 +1,70 @@
-// Product.jsx
-import React from "react"
-import { Container } from "@material-ui/core"
+import React, { useState } from "react"
+import { Container, Button, Modal, Checkbox, FormControlLabel } from "@material-ui/core"
 import * as S from "./style"
 
-function Product({ name, price, description }) {
+function Product({ name, price, description, options, items }) {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [selectedOptions, setSelectedOptions] = useState([])
+
+  const handleOpenModal = () => {
+    setModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setModalOpen(false)
+    setSelectedOptions([])
+  }
+
+  const handleOptionChange = (option) => {
+    const isSelected = selectedOptions.includes(option)
+
+    if (isSelected) {
+      setSelectedOptions(selectedOptions.filter((selectedOption) => selectedOption !== option))
+    } else {
+      setSelectedOptions([...selectedOptions, option])
+    }
+  }
+
+  const handleAddToCart = () => {
+    // 선택된 옵션과 상품 정보를 이용하여 장바구니에 상품을 추가하는 로직을 구현하세요.
+    // 장바구니에 상품을 추가하는 함수 또는 API 호출 등을 여기에 작성합니다.
+    console.log("Add to cart:", name, selectedOptions)
+  }
+
   return (
     <Container>
-      <S.ProductWrapper onClick={() => {}}>
+      <S.ProductWrapper onClick={handleOpenModal}>
+        <S.ProductName>
+          {name}
+          <br />
+          {price}원
+        </S.ProductName>
         <S.ProductImg src="/img/logo.png" size={30} />
-
-        <S.ProductName>{name}</S.ProductName>
-        <S.ProductPrice>{price}원</S.ProductPrice>
       </S.ProductWrapper>
+
+      <Modal open={modalOpen} onClose={handleCloseModal}>
+        <S.ModalWrapper>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          {options && options.length > 0 && (
+            <>
+              <h3>옵션:</h3>
+              {options.map((option) => (
+                <FormControlLabel
+                  key={option}
+                  control={
+                    <Checkbox checked={selectedOptions.includes(option)} onChange={() => handleOptionChange(option)} />
+                  }
+                  label={option}
+                />
+              ))}
+            </>
+          )}
+          {!options || (options.length === 0 && <p>사용 가능한 옵션이 없습니다.</p>)}
+          <Button onClick={handleAddToCart}>장바구니</Button>
+          <Button onClick={handleCloseModal}>닫기</Button>
+        </S.ModalWrapper>
+      </Modal>
     </Container>
   )
 }
--- src/components/product/style.js
+++ src/components/product/style.js
@@ -31,3 +31,46 @@ export const ProductName = styled.h1`
 export const ProductPrice = styled.h3`
   align-items: center;
 `
+
+// style.js
+
+export const ModalWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  max-width: 400px;
+  width: 100%;
+  max-height: 80vh;
+  overflow-y: auto;
+
+  h2 {
+    margin-top: 0;
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+
+  h3 {
+    margin-top: 20px;
+  }
+
+  ul {
+    margin-bottom: 20px;
+    padding-left: 20px;
+  }
+
+  button {
+    background-color: #2196f3;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+`
