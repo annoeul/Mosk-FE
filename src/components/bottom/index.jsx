2e6c03c3032c5c0940a0b74c9c9ff0e5d67c2e5c
annoeul <shdmf1137@gamil.com>
2023-05-15 14:25:02 +0900

    Design: 디자인 작업 중

--- src/components/bottom/index.jsx
+++ src/components/bottom/index.jsx
@@ -1,11 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { BottomWrapper } from "./style"
-import { Button } from "@material-ui/core"
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
+import Badge from "material-ui/Badge"
 
 function Bottom() {
+  const [cart, setCart] = useState(0)
+  const countHandle = () => {
+    setCart(cart + 1)
+  }
   return (
     <BottomWrapper>
-      <Button style={{ backgroundColor: "green", width: "100%" }}>장바구니</Button>
+      <ShoppingCartOutlinedIcon style={{ margin: "10px" }} fontSize="large" onClick={() => console.log("aa")} />
     </BottomWrapper>
   )
 }
--- src/components/bottom/style.js
+++ src/components/bottom/style.js
@@ -1,6 +1,10 @@
 import { styled } from "styled-components"
 
 export const BottomWrapper = styled.div`
-  text-align: center;
-  /* border: 2px solid black; */
+  position: relative;
+  text-align: right;
+  /* padding-bottom: 130px; */
+  bottom: 60px;
+  /* right: 10px; */
+  border: 1px solid black;
 `
