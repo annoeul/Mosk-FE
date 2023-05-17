abe9fd5a84bc1dbb8ffc12335a6f530af7889579
annoeul <shdmf1137@gamil.com>
2023-05-17 09:14:48 +0900

    feat: 로그인 회원가입 라우터 다시 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,20 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import KioskMain from "../pages/kioskmain"
-
-// import LoginPage from "../pages/loginPage"
-// import RegisterPage from "../pages/registerPage"
-// import HomePage from "../pages/homePage"
-// import StoreRegisterPage from "../pages/storeRegisterPage"
+import CartPage from "../pages/cart"
+import LoginPage from "../pages/dashPage/loginPage"
+import RegisterPage from "../pages/dashPage/registerPage"
+import StoreRegisterPage from "../pages/dashPage/storeRegisterPage"
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<KioskMain />} />
+        <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/register/store" element={<StoreRegisterPage />} />  */}
-        <Route path="/" element={<KioskMain />} />
+        <Route path="/register/store" element={<StoreRegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
