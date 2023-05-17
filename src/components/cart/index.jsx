e17c04dcddc195656bc6f9a78bde58fd6dafa13e
annoeul <shdmf1137@gamil.com>
2023-05-17 20:15:46 +0900

    feat: cart.jsx 수정

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -3,13 +3,9 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
 import * as S from "./style"
 
 function Cart() {
-  const [cart, setCart] = useState(0)
-  const countHandle = () => {
-    setCart(cart + 1)
-  }
   return (
     <S.CartWrapper>
-      <ShoppingCartOutlinedIcon style={{ margin: "10px" }} fontSize="large" onClick={() => console.log("aa")} />
+      <ShoppingCartOutlinedIcon style={{ margin: "10px" }} fontSize="large" />
     </S.CartWrapper>
   )
 }
