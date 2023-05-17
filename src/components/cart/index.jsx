2bec45216d1b4f7fe0efafb866eecaa9f146eb41
annoeul <shdmf1137@gmail.com>
2023-05-18 01:14:09 +0900

    Feat:장바구니 기능 1차 구현

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,12 +1,51 @@
+// Cart.jsx
 import React, { useState } from "react"
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
+import { Modal, Button } from "@material-ui/core"
 import * as S from "./style"
 
-function Cart() {
+function Cart({ cartItems }) {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleCartClick = () => {
+    setModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setModalOpen(false)
+  }
+
+  // 총 가격 계산
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0)
+
   return (
-    <S.CartWrapper>
-      <ShoppingCartOutlinedIcon style={{ margin: "10px" }} fontSize="large" />
-    </S.CartWrapper>
+    <>
+      <S.CartWrapper onClick={handleCartClick}>
+        <ShoppingCartOutlinedIcon style={{ margin: "10px" }} fontSize="large" />
+        {cartItems.length > 0 && <S.CartCount>{cartItems.length}</S.CartCount>}
+      </S.CartWrapper>
+
+      <Modal open={modalOpen} onClose={handleCloseModal}>
+        <div style={{ backgroundColor: "white", padding: "20px", borderRadius: "10px" }}>
+          <h2>장바구니</h2>
+          {cartItems.length > 0 ? (
+            <div>
+              <ul>
+                {cartItems.map((item, index) => (
+                  <li key={index}>
+                    {item.name} - {item.options.join(", ")} ({item.price}원)
+                  </li>
+                ))}
+              </ul>
+              <p>총 가격: {totalPrice}원</p>
+            </div>
+          ) : (
+            <p>장바구니가 비어 있습니다.</p>
+          )}
+          <Button onClick={handleCloseModal}>닫기</Button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
--- src/components/cart/style.js
+++ src/components/cart/style.js
@@ -6,5 +6,17 @@ export const CartWrapper = styled.div`
   /* padding-bottom: 130px; */
   bottom: 60px;
   /* right: 10px; */
-  border: 1px solid black;
+  /* border: 1px solid black; */
+`
+export const CartCount = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #ff0000;
+  color: #ffffff;
+  text-align: center;
+  font-size: 12px;
+  line-height: 20px;
+  margin-right: 20px;
 `
