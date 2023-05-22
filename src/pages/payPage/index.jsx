f434d015c7322c3f4bdc01b74ea5e94fd4ef6b7e
annoeul <shdmf1137@gamil.com>
2023-05-22 10:21:43 +0900

    Feat: payPage toss payments 연동

--- src/pages/payPage/index.jsx
+++ src/pages/payPage/index.jsx
@@ -1,7 +1,67 @@
-import React from "react"
+import { useEffect, useRef, useState } from "react"
+import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk"
+import { nanoid } from "nanoid"
 
-function PayPage() {
-  return <div>PayPage</div>
+const clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq"
+const customerKey = "YbX2HuSlsC9uVJW6NMRMj"
+
+export default function App() {
+  const paymentWidgetRef = useRef(null)
+  const paymentMethodsWidgetRef = useRef(null)
+  const [price, setPrice] = useState(50_000)
+
+  useEffect(() => {
+    ;(async () => {
+      const paymentWidget = await loadPaymentWidget(clientKey, customerKey)
+
+      const paymentMethodsWidget = paymentWidget.renderPaymentMethods("#payment-widget", price)
+
+      paymentWidgetRef.current = paymentWidget
+      paymentMethodsWidgetRef.current = paymentMethodsWidget
+    })()
+  }, [])
+
+  useEffect(() => {
+    const paymentMethodsWidget = paymentMethodsWidgetRef.current
+
+    if (paymentMethodsWidget == null) {
+      return
+    }
+
+    paymentMethodsWidget.updateAmount(price, paymentMethodsWidget.UPDATE_REASON.COUPON)
+  }, [price])
+
+  const handlePayment = async () => {
+    const paymentWidget = paymentWidgetRef.current
+
+    try {
+      await paymentWidget?.requestPayment({
+        orderId: nanoid(),
+        orderName: "토스 티셔츠 외 2건",
+        customerName: "김토스",
+        customerEmail: "customer123@gmail.com",
+        successUrl: `${window.location.origin}/success`,
+        failUrl: `${window.location.origin}/fail`,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <div>
+      <h1>주문서</h1>
+      <div id="payment-widget" />
+      <div>
+        <input
+          type="checkbox"
+          onChange={(event) => {
+            setPrice(event.target.checked ? price - 5_000 : price + 5_000)
+          }}
+        />
+        <label>5,000원 할인 쿠폰 적용</label>
+      </div>
+      <button onClick={handlePayment}>결제하기</button>
+    </div>
+  )
 }
-
-export default PayPage
