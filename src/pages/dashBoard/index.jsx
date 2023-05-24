d214b002d4d75550733f36da86dbd0509c742f0a
annoeul <shdmf1137@gamil.com>
2023-05-24 16:30:24 +0900

    Feat: dashBoard page 추가

--- src/pages/dashBoard/index.jsx
+++ src/pages/dashBoard/index.jsx
@@ -1,12 +1,29 @@
-import React from "react"
-import DashMain from "../../components/dashboard/dashHome"
+// /* eslint-disable no-unused-vars */
+import { ColorModeContext, useMode } from "../../theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Route, Routes, Outlet } from "react-router"
+import Sidebar from "../../components/global/Sidebar"
+import Topbar from "../../components/global/Topbar"
+import SetMenu from "../../components/dashboard/setMenu"
 
-function DashPages() {
+function DashPage() {
+  const [theme, colorMode] = useMode()
+
   return (
-    <div>
-      <DashMain />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App" style={{ display: "flex" }}>
+          <Sidebar />
+
+          <main className="content">
+            <Topbar />
+            <Outlet />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
-export default DashPages
+export default DashPage
