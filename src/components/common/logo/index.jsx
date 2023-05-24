62f9f2eea7ba20bf34bd4a3cfa9b4ea6f409e477
annoeul <shdmf1137@gamil.com>
2023-05-24 13:19:24 +0900

    Fix: 안불러지던거 고침

--- src/components/common/logo/index.jsx
+++ src/components/common/logo/index.jsx
@@ -2,23 +2,9 @@ import React, { useState, useEffect } from "react"
 import * as S from "./style"
 
 function Logo({ size }) {
-  const [showLogo, setShowLogo] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLogo(false)
-    }, 1500)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     <S.LogoWrapper>
-      {showLogo ? (
-        <S.LogoFadeOut>
-          <S.Logo src="/img/logo.png" size={size} />
-        </S.LogoFadeOut>
-      ) : null}
+      <S.Logo src="/img/logo.png" size={size} />
     </S.LogoWrapper>
   )
 }
