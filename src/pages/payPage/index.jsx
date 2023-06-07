74030d2cdc74a24ea167dda8fd110b5b90eadcc4
annoeul <shdmf1137@gamil.com>
2023-06-07 15:38:21 +0900

    fix

--- src/pages/payPage/index.jsx
+++ src/pages/payPage/index.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useRef, useState } from "react"
-import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk"
+import { loadPaymentWidget, ANONYMOUS } from "@tosspayments/payment-widget-sdk"
 import { nanoid } from "nanoid"
-import { useLocation } from "react-router-dom"
 
-export default function Pay() {
+export default function App() {
   const paymentWidgetRef = useRef(null)
   const paymentMethodsWidgetRef = useRef(null)
-  const [price, setPrice] = useState(50_000)
-  const location = useLocation()
-  const cartItems = location.state?.cartItems || []
+  const [price, setPrice] = useState(50000)
+
+  const clientKey = import.meta.env.VITE_API_KEY_clientKey
 
   useEffect(() => {
     ;(async () => {
-      const paymentWidget = await loadPaymentWidget(clientKey, customerKey)
+      // const paymentWidget = await loadPaymentWidget(clientKey, customerKey);
+      const paymentWidget = await loadPaymentWidget(clientKey, ANONYMOUS)
 
       const paymentMethodsWidget = paymentWidget.renderPaymentMethods("#payment-widget", price)
 
@@ -35,12 +35,6 @@ export default function Pay() {
     const paymentWidget = paymentWidgetRef.current
 
     try {
-      const items = cartItems.map((item) => ({
-        name: item.name,
-        quantity: 1,
-        price: item.price,
-      }))
-
       await paymentWidget?.requestPayment({
         orderId: nanoid(),
         orderName: "토스 티셔츠 외 2건",
@@ -48,7 +42,6 @@ export default function Pay() {
         customerEmail: "customer123@gmail.com",
         successUrl: `${window.location.origin}/success`,
         failUrl: `${window.location.origin}/fail`,
-        items,
       })
     } catch (err) {
       console.log(err)
@@ -60,15 +53,13 @@ export default function Pay() {
       <h1>주문서</h1>
       <div id="payment-widget" />
       <div>
-        <h2>장바구니 내역</h2>
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              <p>{item.name}</p>
-              <p>{item.price}원</p>
-            </li>
-          ))}
-        </ul>
+        <input
+          type="checkbox"
+          onChange={(event) => {
+            setPrice(event.target.checked ? price - 5000 : price + 5000)
+          }}
+        />
+        <label>5,000원 할인 쿠폰 적용</label>
       </div>
       <button onClick={handlePayment}>결제하기</button>
     </div>
