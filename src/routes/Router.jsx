32168da0d352e3c6fc46cef4ff90de494b930bf1
annoeul <shdmf1137@gmail.com>
2023-06-14 14:38:51 +0900

    Design: 초기 접속 로고 디자인

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,10 +6,14 @@ import Logo from "../components/common/logo"
 
 function Router() {
   const [showLogo, setShowLogo] = useState(true)
+  const [showKioskMain, setShowKioskMain] = useState(false)
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLogo(false)
+      setTimeout(() => {
+        setShowKioskMain(true)
+      }, 500) // 로고가 사라진 후 0.5초 후에 KioskMain이 나타나도록 지연 설정
     }, 2000)
 
     return () => clearTimeout(timer)
@@ -17,24 +21,29 @@ function Router() {
 
   return (
     <BrowserRouter>
-      {showLogo ? (
+      <div style={{ position: "relative", height: "100vh" }}>
         <div
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            backgroundColor: "#ddd",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: `translate(-50%, -50%) scale(${showLogo ? 1 : 0})`,
+            transition: "transform 1s, opacity 1s", // 트랜지션 설정 (1초 동안 페이드 아웃 및 크기 변환)
+            opacity: showLogo ? 1 : 0,
           }}
         >
           <Logo size={100} />
         </div>
-      ) : (
-        <Routes>
-          <Route path="/" element={<KioskMain />} />
-          <Route path="/pay" element={<PayPage />} />
-        </Routes>
-      )}
+
+        {showKioskMain && (
+          <div>
+            <Routes>
+              <Route path="/" element={<KioskMain />} />
+              <Route path="/pay" element={<PayPage />} />
+            </Routes>
+          </div>
+        )}
+      </div>
     </BrowserRouter>
   )
 }
